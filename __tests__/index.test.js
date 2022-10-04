bf5ebf532c1f35586852c38c1e6e774969634438
AleksKutsenko <alonse_97@mail.ru>
2022-10-04 21:47:18 +0300

    refactoring code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import genDiff from '../src/index.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
