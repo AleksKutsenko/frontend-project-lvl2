b1a63148018eb9a70db9b31c7b68ec393b55a04b
AleksKutsenko <alonse_97@mail.ru>
2022-10-11 20:29:09 +0300

    code refactoring from code review 5

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import parse from './parsers.js';
 import buildDiff from './buildDiff.js';
 import format from './formatters/index.js';
 
-const getPath = (filepath) => path.resolve(process.cwd(), filepath);
-const readFile = (filepath) => fs.readFileSync(getPath(filepath), 'utf-8');
+const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const readFile = (filepath) => fs.readFileSync(buildFullPath(filepath), 'utf-8');
 const getFormat = (filepath) => path.extname(filepath).slice(1);
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const content1 = readFile(filepath1);
   const content2 = readFile(filepath2);
-
   const data1 = parse(content1, getFormat(filepath1));
   const data2 = parse(content2, getFormat(filepath2));
-
   const buildDiffTree = buildDiff(data1, data2);
   return format(buildDiffTree, formatName);
 };
